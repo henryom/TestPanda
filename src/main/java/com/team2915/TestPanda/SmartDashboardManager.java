dfe5229a14964374cf2a8c80e07cd7ed4b506601
Andrew <inbox.Andrew2k@gmail.com>
2017-05-16 14:10:51 -0700

    I didn't do anything

--- src/main/java/com/team2915/TestPanda/SmartDashboardManager.java
+++ src/main/java/com/team2915/TestPanda/SmartDashboardManager.java
@@ -3,19 +3,27 @@ package com.team2915.TestPanda;
 import com.team2915.TestPanda.util.VisionTarget;
 import edu.wpi.first.wpilibj.PowerDistributionPanel;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 
 /**
  * Created by Henry on 5/10/17.
  */
 public class SmartDashboardManager {
+    
+    updateSmartDashboard();
 
     PowerDistributionPanel pdp = new PowerDistributionPanel();
+    
+    private void smartDashboardInitialSetup() {
+       
+    }
 
     public SmartDashboardManager() {
     }
 
     public void updateSmartDashboard() {
         SmartDashboard.putBoolean("Did we copy 254?", true); //Example for Andrew
+        SmartDashboard.putBoolean("Are we better than 1540?", true); //Example for Henry
         SmartDashboard.putNumber("PDP Voltage", pdp.getVoltage());
         SmartDashboard.putNumber("NAVX Angle", Robot.driveTrain.getHeading());
     }
